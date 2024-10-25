4c43a8c7217223ef9659658188dbf86388bc7ff5
imthi <imthi@imthiyas>
2024-10-25 15:52:19 +0530

    Second Commit

---
+++ C2TC_IMTHIYAS/src/com/tns/ifet/day1/IdentifiersDemo.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.day1;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int $number=20;
+		System.out.println("value of the number varibale is:"+$number);
+		String studentName="Aniket";
+		System.out.println("Value is:"+ studentName);
+	}
+}
