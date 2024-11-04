7b98386693da362b10f3858e97e069fa534d1330
imthi <imthi@imthiyas>
2024-11-04 15:53:02 +0530

    day3 commit

--- C2TC_IMTHIYAS/src/com/tns/ifet/day2/IfDemo.java
+++ C2TC_IMTHIYAS/src/com/tns/ifet/day2/IfDemo.java
@@ -4,7 +4,7 @@ public class IfDemo {
 
 	public static void main(String[] args) {
 		int x = 5;
-		if (x != 5)
-		System.out.println("Value of x is not 5");
+		if (x != 8)
+		System.out.println(" Value of x is not5 ");
 		}
 		}
